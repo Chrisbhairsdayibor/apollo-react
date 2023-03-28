6873145da9cd5ed47a3c16b6b83e9440d0c7ea28
Chrisbhairsdayibor <bastyhairs910@yahoo.com>
2023-03-28 09:51:15 +0000

    Router added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+import { BrowserRouter } from 'react-router-dom';
+
+const client = new ApolloClient({
+  uri: 'https://countries.trevorblades.com/',
+  cache: new InMemoryCache(),
+});
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ApolloProvider client={client}>
+    <BrowserRouter>
+<App/>
+</BrowserRouter>
+</ApolloProvider>,
+</React.StrictMode>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
